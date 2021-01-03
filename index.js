b1f97b4f8eadfaecfe6dfa7daf01e6d0521fb5d6
Joseph Lynn <josephlynn94@gmail.com>
2021-01-03 10:01:22 -0500

    started javascript setup for timer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const settingsToggle = document.querySelector('#js-settings__icon--toggle');
 const settingsCloseBtn = document.querySelector('#settings__close-btn');
 const settingsModal = document.querySelector('#js-settings__modal');
+const timerDisplay = document.querySelector('.min-sec');
 
 // Open modal on the settings button click
 settingsToggle.addEventListener('click', () => {
@@ -22,3 +23,50 @@ window.addEventListener('click', (event) => {
     ? settingsModal.classList.remove('show-modal')
     : false;
 });
+
+// set the total seconds
+let totalSeconds = 500;
+// check the seconds elasped
+let secondsElasped = 65;
+
+const getFormatedMinutes = () => {
+  // check how many seconds are left
+  let secondsLeft = totalSeconds - secondsElasped;
+  // check remaining minutes by dividing the secondsLeft by 60
+  let minutesLeft = Math.floor(secondsLeft / 60);
+
+  // set the formated mintes
+  let formattedMinutes;
+  // format minutes if less than 10
+  if (minutesLeft < 0) {
+    formattedMinutes = `0${minutesLeft}`;
+  } else {
+    formattedMinutes = minutesLeft;
+  }
+
+  console.log(formattedMinutes);
+  return formattedMinutes;
+};
+
+const getFormatedSeconds = () => {
+  // get the remainder seconds
+  let secondsLeft = (totalSeconds - secondsElasped) % 60;
+
+  // set formated seconds
+  let formattedSeconds;
+  // format seconds if less than 10
+  if (secondsLeft < 10) {
+    formattedSeconds = `0${secondsLeft}`;
+  } else {
+    formattedSeconds = secondsLeft;
+  }
+
+  console.log(formattedSeconds);
+  return formattedSeconds;
+};
+
+const renderTime = () => {
+  timerDisplay.textContent = `${getFormatedMinutes()}:${getFormatedSeconds()}`;
+};
+
+renderTime();
